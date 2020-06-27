b0c62108c9316daabd82d2dc1afb79a7edd97f86
Lawrence <lawrencegodfrey58@gmail.com>
2020-06-27 23:57:26 +0200

    implemented feedforward for single example

--- Network.cpp
+++ Network.cpp
@@ -3,24 +3,32 @@
 namespace Neuron {
 
 Network::Network() : 
-    dimensions (std::vector<int>    (0)),
-    layers     (std::vector<Matrix> (0)),
-    weights    (std::vector<Matrix> (0)),
-    bias       (std::vector<Matrix> (0)) {}
+    dimensions (std::vector<int>        (0)),
+    layers     (std::vector<Matrix>     (0)),
+    weights    (std::vector<Matrix>     (0)),
+    bias       (std::vector<Matrix>     (0)) {}
 
 Network::Network(const std::vector<int> & dimensions) : 
     dimensions (dimensions),
-    layers     (std::vector<Matrix> (dimensions.size())),
-    weights    (std::vector<Matrix> (dimensions.size() - 1)),
-    bias       (std::vector<Matrix> (dimensions.size() - 1))  {
+    layers     (std::vector<Matrix>       (dimensions.size()    )),
+    layers_activated(std::vector<Matrix>  (dimensions.size()    )),
+    weights    (std::vector<Matrix>       (dimensions.size() - 1)),
+    activations(std::vector<Activation *> (dimensions.size() - 1)),
+    bias       (std::vector<Matrix>       (dimensions.size() - 1)),
+    learning_rate (0.01)  
+{
     
     for (int layer = 0; layer < layers.size(); layer++) {
-        layers.at(layer) = std::move(Matrix(1, dimensions.at(layer)));
+        layers.at(layer) = std::move(Matrix(dimensions.at(layer), 1));
+        layers_activated.at(layer) = std::move(Matrix(dimensions.at(layer), 1));
     }
 
+    for (int layer = 0; layer < layers.size() - 1; layer++) 
+        activations.at(layer) = new ReLU();
+
     for (int weight = 0; weight < weights.size(); weight++) {
-        weights.at(weight) = RandomNormalMatrix(dimensions.at(weight), dimensions.at(weight + 1) , 0, 5);        
-        bias.at(weight)    = std::move(Matrix      (1,                     dimensions.at(weight) + 1));
+        weights.at(weight) = std::move(RandomNormalMatrix(dimensions.at(weight),     dimensions.at(weight + 1) , 0, 2));        
+        bias.at(weight)    = std::move(RandomNormalMatrix(dimensions.at(weight + 1), 1                         , 0, 2));
     }
 }
 
@@ -32,20 +40,41 @@ Network::Network(const std::initializer_list<double> & list) :
     bias       (std::vector<Matrix> (list.size() - 1))  {}
 
 
-void Network::feedforward(const vector1D & inputs) {
-    // output_layer = weights.at(last - 1).transpose multiply outputs of previous layer 
-
+void Network::feedforward(const Matrix & inputs) {   
+    int i = 0;
+    
     for (auto & layer : layers) {
-        if (layer == *layers.begin()) {
+        if (i == 0) {
             layer = inputs;
+            std::cout << "first " << layer << std::endl;
         }
         else {
-            layer = weights.at(i - 1).Transpose().dot(layers.at(i - 1)) + bias;
+            std::cout << "weights transposed: " << weights.at(i - 1).Transpose() << std::endl;
+
+            layer = weights.at(i - 1).Transpose().dot(layers.at(i - 1)) + bias.at(i - 1);
+            std::cout << "i: " << i << std::endl;
+            std::cout << "before activate" << std::endl << std::flush;
+            layers_activated.at(i) = activations.at(i - 1)->activate(layer); 
+            std::cout << "after activate" << std::endl << std::flush;
+
         }
+
+        i++;
     }
 }
 
+void Network::backpropagate(const Matrix & correct_outputs) {
+    Matrix error = Cost(getOutputLayer(), correct_outputs).multiply(activations.at(activations.size() - 1)->activate_prime(layers.at(layers.size() - 1)));
 
+    for (int i = layers.size() - 2; i > 0; i--) {
+        error = (weights.at(i + 1).Transpose().dot(error)).multiply(activations.at(i)->activate_prime(layers.at(i)));
+    }
+
+}
+
+Matrix Network::getOutputLayer() const {
+    return layers_activated.at(layers_activated.size() - 1);
+}
 
 Matrix & Network::operator = (const std::initializer_list<double> & list) { 
 
@@ -57,7 +86,7 @@ Matrix & Network::operator = (const std::initializer_list<double> & list) {
 }
 
 void Network::train(vector2D inputs, vector2D output) {
-
+    feedforward(inputs);
 }
 
 
@@ -68,6 +97,11 @@ vector1D Network::predict(vector1D inputs, vector1D output) const {
 
 double Network::test(vector2D inputs, vector2D output) const {
 
+}
+
+vector2D Network::getOutput() const {
+    std::cout << "output layer: " << getOutputLayer() << std::endl;
+    return getOutputLayer().toVector();
 }
 
 
@@ -89,6 +123,29 @@ std::vector<int> Network::Dimensions() const {
 
 }
 
+Matrix Cost(const Matrix & outputs, const Matrix & correct_outputs) {
+    if (outputs.shape() != correct_outputs.shape()) 
+        throw MatrixDimensionError();
+    
+    Matrix output_matrix (outputs);
+
+    for (unsigned int i = 0; i < outputs.size(); i++) 
+        output_matrix(i) = outputs(i) - correct_outputs(i);
+
+    return output_matrix;
+}
+
+Matrix squaredCost(const Matrix & outputs, const Matrix & correct_outputs) {
+    if (outputs.shape() != correct_outputs.shape()) 
+        throw MatrixDimensionError();
+    
+    Matrix output_matrix (outputs);
+
+    for (unsigned int i = 0; i < outputs.size(); i++) 
+        output_matrix(i) = 0.5 * std::pow(outputs(i) - correct_outputs(i), 2);
+
+    return output_matrix;
+}
 
 std::pair<vector2D, vector2D> train_test_split(vector2D, const double & test_size) {
 
--- Network.h
+++ Network.h
@@ -10,37 +10,58 @@ namespace Neuron {
 using vector2D = std::vector<std::vector<double>>; 
 using vector1D = std::vector<double>;
 
+class Activation;
+class Step;
+class Sigmoid;
+class ReLU;
+
 class Network {
-    std::vector<int> dimensions;
+    
+    std::vector<int>        dimensions;
 
-    std::vector<Matrix> layers;
-    std::vector<Matrix> weights;
-    std::vector<Matrix> bias;
+    std::vector<Matrix>       layers;
+    std::vector<Matrix>       layers_activated;
+    std::vector<Matrix>       weights;
+    std::vector<Matrix>       bias;
 
-    void feedforward(const vector1D & inputs);
-    void backpropagate();
+    std::vector<Activation *> activations;
 
+    double learning_rate;
+    
+    void backpropagate(const Matrix & correct_outputs);
+    Matrix getOutputLayer() const;
+   
     public:
+
+    void feedforward(const Matrix & inputs);
         Network();
         Network(const std::vector<int> & dimensions);
         Network(const std::initializer_list<double> & list);
 
         Matrix & operator = (const std::initializer_list<double> & list);
 
-        void train(vector2D inputs, vector2D output); 
+        void     train  (vector2D inputs, vector2D output); 
         vector1D predict(vector1D inputs, vector1D output) const;    
-        double test(vector2D inputs, vector2D output) const;
+        double   test   (vector2D inputs, vector2D output) const;
         
         template <typename T>
         void addLayer(const int & layer_size);
 
-        std::vector<Matrix> getWeights() const;
-        std::vector<Matrix> getLayers() const;
+        template <typename T>
+        void setLayer(const int & layer);
+
+        vector2D getOutput() const;
+
+        std::vector<Matrix> getWeights()    const;
+        std::vector<Matrix> getLayers()     const;
         std::vector<Matrix> getBiasVector() const;
 
         std::vector<int> Dimensions() const;
         
 };
+
+Matrix Cost(const Matrix & outputs, const Matrix & correct_outputs);
+Matrix squaredCost(const Matrix & outputs, const Matrix & correct_outputs);
 
 template<typename T>
 void Network::addLayer(const int & layer_size) {
@@ -51,6 +72,81 @@ std::pair<vector2D, vector2D> train_test_split(vector2D, const double & test_siz
 
 vector1D oneHot(vector1D output_layer);
 
+class Activation {
+    public:
+        virtual Matrix activate(const Matrix & layer) {
+            std::cout << "in Activation" << std::endl;
 
+            return layer;
+        }
+
+        virtual Matrix activate_prime(const Matrix & layer) {
+            std::cout << "in Activation prime" << std::endl;
+
+            return layer;
+        }
+};
+
+class Step : public Activation {
+    public:
+        
+        virtual Matrix activate(const Matrix & layer) override {
+            std::cout << "in Step" << std::endl << std::flush;
+            Matrix output_matrix (layer);
+            std::cout << "Layer before step : " << std::endl << layer << std::endl;
+
+            for (unsigned int i = 0; i < layer.size(); i++) 
+                output_matrix(i) = int(output_matrix(i) >= 0.5);
+
+            return output_matrix;
+        }
+
+        virtual Matrix activate_prime(const Matrix & layer) override {
+            return activate(layer);
+        }
+
+};
+
+class Sigmoid : public Activation {
+    public:
+        virtual Matrix activate(const Matrix & layer) override {
+            Matrix output_matrix (layer);
+
+            for (unsigned int i = 0; i < layer.size(); i++) 
+                output_matrix(i) = 1 / (1 + std::exp(-1 * output_matrix(i)));
+
+            return output_matrix;
+        }
+
+        virtual Matrix activate_prime(const Matrix & layer) override {
+            return activate(layer) * (1 - activate(layer));
+        }
+
+};
+
+class ReLU : public Activation {
+    public: 
+        virtual Matrix activate(const Matrix & layer) override {
+            std::cout << "in ReLU" << std::endl << std::flush;
+            Matrix output_matrix (layer);
+            std::cout << "Layer before relu : " << std::endl << layer << std::endl;
+            for (unsigned int i = 0; i < layer.size(); i++) 
+                output_matrix(i) = std::max(0.0, output_matrix(i));
+
+            std::cout << "Layer after relu: " << std::endl << output_matrix << std::endl;
+
+            return output_matrix;
+        }
+
+        virtual Matrix activate_prime(const Matrix & layer) override {
+            Matrix output_matrix (layer);
+
+            for (unsigned int i = 0; i < layer.size(); i++) 
+                output_matrix(i) = int(output_matrix(i) >= 0.5);
+
+            return output_matrix;
+        }
+
+};
 
 }
